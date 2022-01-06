f4b22b4cf2c2f9be633cfd4ffd3ce64442d89c1f
Md Majharul Islam <80042446+majharul-web@users.noreply.github.com>
2022-01-06 09:13:07 +0600

    overall done

---
+++ README.md
@@ -0,0 +1,15 @@
+# Website Name: ToDo App.
+
+Visit MY Website: [ToDo App](https://sazid-honda.web.app/).
+
+## Website info:
+
+- It is a ToDo List Web App.
+- Implemented Email-Password and Google Authentication system using Firebase.
+- Users can sign up and login to their account and can see their own todo lists.
+  They can create, update and delete their todo list.
+- I make this website using HTML,CSS,Material-UI,FontAwesome,Javascript and React for FrontEnd.
+- NodeJs,Express and Mongodb Database Using BackEnd.
+- Use React Router-v6.
+- Applied Private Route.
+- Client Side hosted in Firebase and Server Side Hosted In Heroku.
--- index.js
+++ index.js
@@ -46,17 +46,22 @@ async function run() {
         })
 
 
+        // todo update
         app.put('/update/:id', async (req, res) => {
             const updateDoc = req.body;
             const id = req.params.id;
             const filter = { _id: ObjectId(id) }
-            console.log(updateDoc);
-            // const options = { upsert: true };
-            // const doc = { $set: { updateDoc } }
-            // console.log(filter);
-            // const result = await usersCollections.updateOne(filter, doc, options);
-            // console.log(result);
-            // res.json(result)
+
+            try {
+                const doc = { $set: updateDoc }
+                console.log(filter);
+                const result = await todoCollection.updateMany(filter, doc);
+                res.json(result)
+            }
+            catch (error) {
+                console.log(error);
+            }
+
         })
 
 
